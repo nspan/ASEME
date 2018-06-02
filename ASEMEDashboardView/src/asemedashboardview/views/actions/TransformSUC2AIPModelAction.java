0af6f74d2bccf212ac9f22f00dead0252d877b0b
ringbeerer <efloros0@gmail.com>
2018-06-02 21:52:38 +0300

    fixed overwrite bug

--- ASEMEDashboardView/src/asemedashboardview/views/actions/TransformSUC2AIPModelAction.java
+++ ASEMEDashboardView/src/asemedashboardview/views/actions/TransformSUC2AIPModelAction.java
@@ -1,38 +1,9 @@
 package asemedashboardview.views.actions;
 
-
+import java.util.Objects;
 
 import javax.swing.JOptionPane;
 
-import AIP.AIPFactory;
-import AIP.AIPPackage;
-import AIP.AIPmodel;
-import AIP.Participant;
-import AIP.Protocol;
-import AIP.diagram.edit.parts.AIPmodelEditPart;
-import AIP.diagram.part.AIPDiagramEditor;
-import AIP.diagram.part.AIPDiagramEditorPlugin;
-import SAG.Actor;
-import SAG.Goal;
-import SAG.SAGPackage;
-import SAG.SAGmodel;
-import SUC.Role;
-import SUC.SUCFactory;
-import SUC.SUCPackage;
-import SUC.SUCmodel;
-import SUC.UseCase;
-import SUC.diagram.edit.parts.SUCmodelEditPart;
-import SUC.diagram.part.SUCDiagramEditor;
-import SUC.diagram.part.SUCDiagramEditorPlugin;
-import SUC.diagram.part.SUCDiagramEditorUtil;
-import aseme.transformations.AsemeModelSaveHelper;
-import aseme.transformations.SUC2AIP;
-import asemedashboardview.views.ASEMEAction;
-import asemedashboardview.views.ASEMEFacade;
-import asemedashboardview.views.ASEMEState;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.emf.common.ui.URIEditorInput;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.URI;
@@ -40,13 +11,25 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.Diagnostician;
-import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
 import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+
+import AIP.AIPPackage;
+import AIP.AIPmodel;
+import AIP.diagram.edit.parts.AIPmodelEditPart;
+import AIP.diagram.part.AIPDiagramEditor;
+import AIP.diagram.part.AIPDiagramEditorPlugin;
+import SUC.SUCPackage;
+import SUC.SUCmodel;
+import aseme.transformations.AsemeModelSaveHelper;
+import aseme.transformations.SUC2AIP;
+import asemedashboardview.views.ASEMEAction;
+import asemedashboardview.views.ASEMEFacade;
+import asemedashboardview.views.ASEMEState;
 
 public class TransformSUC2AIPModelAction implements ASEMEAction {
 
@@ -60,7 +43,7 @@ public class TransformSUC2AIPModelAction implements ASEMEAction {
 	@Override
 	public boolean isEnabled() {
 		ASEMEState state = context.getState();
-		if(state.getSUC()==null) {
+		if (state.getSUC() == null) {
 			return false;
 		}
 		return true;
@@ -71,43 +54,46 @@ public class TransformSUC2AIPModelAction implements ASEMEAction {
 		ASEMEState state = context.getState();
 		URI suc = state.getSUC();
 		URI aip = state.getAIP();
+
 		if (aip == null) {
 			aip = suc.trimFileExtension().appendFileExtension("aip"); //$NON-NLS-1$
 			state.setAIP(aip);
-		}
-		else{
-			
-			boolean quest = MessageDialog.openQuestion(context.getShell(), "AIP model exists!" , aip.toString() + " already exists. Do you want to overwrite?");
-			
-			if (!quest){
-				String aipName = suc.trimFileExtension().toString() + "_0";
-				
-				String[] aipPathParts = aipName.split("/") ;
-				
-				String newName = JOptionPane.showInputDialog("Give name", aipPathParts[aipPathParts.length-1]);
+		} else {
 
-				aipPathParts[aipPathParts.length-1] = newName;
-				
+			boolean quest = MessageDialog.openQuestion(context.getShell(), "AIP model exists!",
+					aip.toString() + " already exists. Do you want to overwrite?");
+
+			if (!quest) {
+				String aipName = suc.trimFileExtension().toString() + "_0";
+
+				String[] aipPathParts = aipName.split("/");
+
+				String newName = JOptionPane.showInputDialog("Give name", aipPathParts[aipPathParts.length - 1]);
+
+				if (Objects.isNull(newName)) {
+					return;
+				}
+
+				aipPathParts[aipPathParts.length - 1] = newName;
+
 				String reconstructedPath = "";
-				
+
 				for (int i = 0; i < aipPathParts.length; i++) {
-					
+
 					reconstructedPath += aipPathParts[i] + "/";
 				}
-				
-				reconstructedPath = reconstructedPath.substring(0,reconstructedPath.length()-1);
-				
+
+				reconstructedPath = reconstructedPath.substring(0, reconstructedPath.length() - 1);
+
 				aip = URI.createURI(reconstructedPath).appendFileExtension("aip");
+
 			}
 		}
 
 		ResourceSet resourceSet = new ResourceSetImpl();
 
-		resourceSet
-		.getResourceFactoryRegistry()
-		.getExtensionToFactoryMap()
-		.put(Resource.Factory.Registry.DEFAULT_EXTENSION,
-				new XMIResourceFactoryImpl());
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
+				.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
 
 		// Register the package to ensure it is available during loading.
 		//
@@ -116,51 +102,44 @@ public class TransformSUC2AIPModelAction implements ASEMEAction {
 
 		// load SUC model
 
-		Resource resource = resourceSet.getResource(state.getSUC(), true);	
+		Resource resource = resourceSet.getResource(state.getSUC(), true);
 		SUCmodel sucModel = (SUCmodel) resource.getContents().get(0);
-		
-		try{
-			
+
+		try {
+
 			AIPmodel aipm = SUC2AIP.transformSuc2Aip(sucModel);
 			AsemeModelSaveHelper.saveURI(aipm, aip);
-			
+
 			URI diag = aip.trimFileExtension().appendFileExtension("aipd");
 			this.createDiagram(aipm, diag);
-			MessageDialog.openConfirm(context.getShell(), "SUC2AIP transformation" , "Output : " + aip.toPlatformString(isEnabled()));
-			}
-		catch( Exception e){
+			MessageDialog.openConfirm(context.getShell(), "SUC2AIP transformation",
+					"Output : " + aip.toPlatformString(isEnabled()));
+		} catch (Exception e) {
 			Diagnostic diagn = Diagnostician.INSTANCE.validate(sucModel);
-			if (diagn.getCode() == 0){
-				MessageDialog.openError(context.getShell(), "Error", "Error in validating SUC model" );
-			}
-			else{
-				MessageDialog.openError(context.getShell(), "Error", "Error in SUC2AIP transformation" );
+			if (diagn.getCode() == 0) {
+				MessageDialog.openError(context.getShell(), "Error", "Error in validating SUC model");
+			} else {
+				MessageDialog.openError(context.getShell(), "Error", "Error in SUC2AIP transformation");
 			}
 		}
-		
 
-		
 	}
-	
-	private void createDiagram(AIPmodel aipm, URI diag){
-		Diagram diagram = ViewService.createDiagram(aipm,
-				AIPmodelEditPart.MODEL_ID,
+
+	private void createDiagram(AIPmodel aipm, URI diag) {
+		Diagram diagram = ViewService.createDiagram(aipm, AIPmodelEditPart.MODEL_ID,
 				AIPDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);
-		
+
 		aipm.eResource().getContents().add(diagram);
-		
-		
+
 		AsemeModelSaveHelper.saveURI(aipm.eResource().getContents().get(1), diag);
-		
+
 		try {
-			PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-				.getActivePage().openEditor(new URIEditorInput(diag), AIPDiagramEditor.ID);
+			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(new URIEditorInput(diag),
+					AIPDiagramEditor.ID);
 		} catch (PartInitException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	
 
 }
--- ASEMEDashboardView/src/asemedashboardview/views/actions/TransformSUC2SRMModelAction.java
+++ ASEMEDashboardView/src/asemedashboardview/views/actions/TransformSUC2SRMModelAction.java
@@ -1,21 +1,14 @@
 package asemedashboardview.views.actions;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.Objects;
 
 import javax.swing.JOptionPane;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.ui.URIEditorInput;
-import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.Diagnostician;
-import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
 import org.eclipse.gmf.runtime.notation.Diagram;
@@ -25,26 +18,15 @@ import org.eclipse.ui.PlatformUI;
 
 import AIP.AIPPackage;
 import AIP.AIPmodel;
-import AIP.Participant;
-import AIP.diagram.edit.parts.AIPmodelEditPart;
-import AIP.diagram.part.AIPDiagramEditor;
-import AIP.diagram.part.AIPDiagramEditorPlugin;
-import SRM.Activity;
-import SRM.Capability;
-import SRM.Role;
 import SRM.SRMFactory;
 import SRM.SRMPackage;
 import SRM.SRMmodel;
 import SRM.diagram.edit.parts.SRMmodelEditPart;
 import SRM.diagram.part.SRMDiagramEditor;
 import SRM.diagram.part.SRMDiagramEditorPlugin;
-import SUC.SUCFactory;
 import SUC.SUCPackage;
 import SUC.SUCmodel;
-import SUC.UseCase;
-import SUC.diagram.part.SUCDiagramEditorUtil;
 import aseme.transformations.AsemeModelSaveHelper;
-import aseme.transformations.SUC2AIP;
 import aseme.transformations.SUC2SRM;
 import asemedashboardview.views.ASEMEAction;
 import asemedashboardview.views.ASEMEFacade;
@@ -62,8 +44,8 @@ public class TransformSUC2SRMModelAction implements ASEMEAction {
 	@Override
 	public boolean isEnabled() {
 		ASEMEState state = context.getState();
-		//TODO : Verify if AIP also needs to be enabled
-		if(state.getSUC()==null ) {
+		// TODO : Verify if AIP also needs to be enabled
+		if (state.getSUC() == null) {
 			return false;
 		}
 		return true;
@@ -71,122 +53,116 @@ public class TransformSUC2SRMModelAction implements ASEMEAction {
 
 	@Override
 	public void run() {
-		ASEMEState state = context.getState(); 
+		ASEMEState state = context.getState();
 		URI suc = state.getSUC();
 		URI srm = state.getSRM();
 		URI aip = state.getAIP();
-		
-		
+
 		if (srm == null) {
 			srm = suc.trimFileExtension().appendFileExtension("srm"); //$NON-NLS-1$
 			state.setSRM(srm);
-		}
-		else{
-			
-			boolean quest = MessageDialog.openQuestion(context.getShell(), "SRM model exists!" , srm  + " already exists. Do you want to overwrite?");
-			
-			if (!quest){
-				String srmName = suc.trimFileExtension().toString() + "_0";
-				
-				
-				String[] srmPathParts = srmName.split("/") ;
-				
-				String newName = JOptionPane.showInputDialog("Give name", srmPathParts[srmPathParts.length-1]);
+		} else {
 
-				srmPathParts[srmPathParts.length-1] = newName;
-				
+			boolean quest = MessageDialog.openQuestion(context.getShell(), "SRM model exists!",
+					srm + " already exists. Do you want to overwrite?");
+
+			if (!quest) {
+				String srmName = suc.trimFileExtension().toString() + "_0";
+
+				String[] srmPathParts = srmName.split("/");
+
+				String newName = JOptionPane.showInputDialog("Give name", srmPathParts[srmPathParts.length - 1]);
+
+				if (Objects.isNull(newName)) {
+					return;
+				}
+
+				srmPathParts[srmPathParts.length - 1] = newName;
+
 				String reconstructedPath = "";
-				
+
 				for (int i = 0; i < srmPathParts.length; i++) {
-					
+
 					reconstructedPath += srmPathParts[i] + "/";
 				}
-				
-				reconstructedPath = reconstructedPath.substring(0,reconstructedPath.length()-1);
-				
+
+				reconstructedPath = reconstructedPath.substring(0, reconstructedPath.length() - 1);
+
 				srm = URI.createURI(reconstructedPath).appendFileExtension("srm");
 			}
 		}
-		
-		
+
 		ResourceSet resourceSet = new ResourceSetImpl();
-		resourceSet
-		.getResourceFactoryRegistry()
-		.getExtensionToFactoryMap()
-		.put(Resource.Factory.Registry.DEFAULT_EXTENSION,
-				new XMIResourceFactoryImpl());
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
+				.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
 
 		resourceSet.getPackageRegistry().put(SUCPackage.eNS_URI, SUCPackage.eINSTANCE);
 		resourceSet.getPackageRegistry().put(SRMPackage.eNS_URI, SRMPackage.eINSTANCE);
-		
+
 		resourceSet.getPackageRegistry().put(AIPPackage.eNS_URI, AIPPackage.eINSTANCE);
 
 		Resource resource = resourceSet.getResource(suc, true);
 		SUCmodel sucModel = (SUCmodel) resource.getContents().get(0);
-		
-		//Resource newResource = resourceSet.createResource(state.getSRM());
+
+		// Resource newResource = resourceSet.createResource(state.getSRM());
 		SRMmodel srmModel = SRMFactory.eINSTANCE.createSRMmodel();
-		//newResource.getContents().add(srmModel);
-		
-		//SUC2SRM tr = new SUC2SRM();
-		
-		//if (nosrm){
-		
-		try{
+		// newResource.getContents().add(srmModel);
+
+		// SUC2SRM tr = new SUC2SRM();
+
+		// if (nosrm){
+
+		try {
 			srmModel = SUC2SRM.transformSuc2srm(sucModel, srmModel);
 			context.updateStatus();
+		} catch (Exception e) {
+			// Diagnostic diag = Diagnostician.INSTANCE.validate(sucModel);
+			// if (diag.getCode() == 0){
+			MessageDialog.openError(context.getShell(), "Error",
+					"Error in validating SUC model\n Possible triggering cause : \n Capability_X=Activity_Y");
 		}
-		catch( Exception e){
-			//Diagnostic diag = Diagnostician.INSTANCE.validate(sucModel);
-			//if (diag.getCode() == 0){
-				MessageDialog.openError(context.getShell(), "Error", "Error in validating SUC model\n Possible triggering cause : \n Capability_X=Activity_Y" );
-			}
-			
-		
-		
-		
-		if (aip!= null){
-			Resource aipResource = resourceSet.getResource(aip, true);//resourceSet.getResource(URI.createPlatformResourceURI(temp.getFullPath().toString(), true), true);
+
+		if (aip != null) {
+			Resource aipResource = resourceSet.getResource(aip, true);// resourceSet.getResource(URI.createPlatformResourceURI(temp.getFullPath().toString(),
+																		// true),
+																		// true);
 			AIPmodel aipModel = (AIPmodel) aipResource.getContents().get(0);
-			
-			try{
+
+			try {
 				srmModel = SUC2SRM.srmImportAip(srmModel, aipModel);
+			} catch (Exception e) {
+				MessageDialog.openError(context.getShell(), "Error", "Error importing AIP to SRM model");
 			}
-			catch( Exception e){
-				MessageDialog.openError(context.getShell(), "Error", "Error importing AIP to SRM model" );
-			}
-						
+
 		}
-		
-		
-		URI test = suc.trimFileExtension().appendFileExtension("srm"); 
-		
-		
-		
+
+		URI test = suc.trimFileExtension().appendFileExtension("srm");
+
 		AsemeModelSaveHelper.saveURI(srmModel, test);
-		
+
 		URI diag = test.trimFileExtension().appendFileExtension("fg");
 		this.createDiagram(srmModel, diag);
 		
+		MessageDialog.openConfirm(context.getShell(), "SUC2SRM transformation",
+				"Output : " + srm.toPlatformString(isEnabled()));
+
 	}
-	
-	private void createDiagram(SRMmodel srmm, URI diag){
-		Diagram diagram = ViewService.createDiagram(srmm,
-				SRMmodelEditPart.MODEL_ID,
+
+	private void createDiagram(SRMmodel srmm, URI diag) {
+		Diagram diagram = ViewService.createDiagram(srmm, SRMmodelEditPart.MODEL_ID,
 				SRMDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);
-		
+
 		srmm.eResource().getContents().add(diagram);
-		
-		
+
 		AsemeModelSaveHelper.saveURI(srmm.eResource().getContents().get(1), diag);
-		
+
 		try {
-			PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-				.getActivePage().openEditor(new URIEditorInput(diag), SRMDiagramEditor.ID);
+			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(new URIEditorInput(diag),
+					SRMDiagramEditor.ID);
 		} catch (PartInitException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 }
